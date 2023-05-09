ae060a76bda6a3b72f64343dec82a0ffd1528ffc
kulumbo10 <124624241+kulumbo10@users.noreply.github.com>
2023-05-08 21:43:13 -0400

    Layout 80% done
    nav bar - feito
    body 80%

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,10 +1,68 @@
 <?php require APPROOT . '/views/includes/header.php'; ?>
 
-<div class="jumbotron jumbotron-fluid">
-    <div class="container">
-        <h1 class="display-3"><?= $data['title'] ?></h1>
-        <p class="lead"><?= $data['description']; ?></p>
-    </div>
+
+
+
+
+  <div class="procura">
+      <h3>PROCURAR UM PROFISSIONAL</h3>
+      <input type="text" class="input" placeholder="Php, Java, C#, Python, Javascript"> <img  src="lupa120.jpg" alt="">
+      
+  </div> <br>
+
+
+  <div class="container2 row align-items-center">
+    <div class="row row justify-content-between">
+      
+    <div class="col-sm">
+      <div class="card card text-white bg-dark mb-3" style="width: 16rem;">
+  <img class="card-img-top" src="ex01.jpeg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Jonh Ferdinand</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
 </div>
+        card1
+      </div>
+
+      <div class="col-sm">
+      <div class="card card text-white bg-dark mb-3" style="width: 16rem;">
+  <img class="card-img-top" src="ex02.jpeg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Ernesto Mateus</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+        card2
+      </div>
+
+      <div class="col-sm">
+      <div class="card card text-white bg-dark mb-3" style="width: 16rem;" >
+  <img class="card-img-top" src="ex03.png.jpeg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Roberto Carlos</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+        card3
+      </div>
+    </div>
+  </div>
+
+
+ 
+ 
+
+ 
+
+
+
+
+
+
+
 
 <?php require APPROOT . '/views/includes/footer.php'; ?>
--- app/views/includes/header.php
+++ app/views/includes/header.php
@@ -13,7 +13,28 @@
         <link rel="stylesheet" href="<?= URLROOT; ?>css/bootstrap-4.0.0/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="<?= URLROOT; ?>css/fontawesome-free-6.4.0-web/css/all.min.css">
         <link rel="stylesheet" href="<?= URLROOT; ?>/css/style.css">
+        
     </head>
+
+      
+   
+
     <body>
+
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Cadastrar um CV</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
         <?php #require APPROOT . '/views/inc/navbar.php'; ?>
         <div class="container">
