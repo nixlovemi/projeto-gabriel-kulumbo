7ca927861414680068e5a7feab9fc429e93a5f7c
Leandro Parra <nixlovemi@gmail.com>
2023-05-18 18:50:00 -0400

    Revert "config on git ignore"
    This reverts commit cf7ccfcc9bddfe3cace1b1cb0cb40f186e76af84.

--- app/config/config.php
+++ app/config/config.php
@@ -1,15 +1,15 @@
 <?php
 
 // DB Params
-define('DB_HOST', 'mysql');
+define('DB_HOST', 'localhost');
 define('DB_PORT', '3306');
-define('DB_USER', 'sandbox_user');
-define('DB_PASS', 'passpass');
+define('DB_USER', 'root');
+define('DB_PASS', '');
 define('DB_NAME', 'sandbox');
 
 // App Root
 define('APPROOT', dirname(dirname(__FILE__)));
-define('URLROOT', 'http://127.0.0.1:8001/');
+define('URLROOT', 'http://127.0.0.1/projeto-gabriel-kulumbo/');
 
 // Site Name
 define('SITENAME', 'Projeto');
