9a42ce0736c756f79fd35ef67aba67a141226803
Leandro Parra <nixlovemi@gmail.com>
2023-05-18 22:56:40 -0400

    ajustes no layout

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,68 +1,50 @@
 <?php require APPROOT . '/views/includes/header.php'; ?>
 
+<div class="procura margin-bottom-padrao bg-cinza-padrao">
+  <h3>PROCURAR UM PROFISSIONAL</h3>
+  <div class="input-group mb-3">
+    <input type="text" class="form-control" placeholder="Pesquisar ..." aria-label="Pesquisar ..." aria-describedby="basic-addon2">
+    <div class="input-group-append">
+      <button class="btn btn-primary" type="button">Pesquisar</button>
+    </div>
+  </div>
+</div>
 
-
-
-
-  <div class="procura">
-      <h3>PROCURAR UM PROFISSIONAL</h3>
-      <input type="text" class="input" placeholder="Php, Java, C#, Python, Javascript"> <img  src="lupa120.jpg" alt="">
-      
-  </div> <br>
-
-
-  <div class="container2 row align-items-center">
-    <div class="row row justify-content-between">
-      
+<div class="container2 row align-items-center bg-cinza-padrao">
+  <div class="row justify-content-between">
     <div class="col-sm">
-      <div class="card card text-white bg-dark mb-3" style="width: 16rem;">
-  <img class="card-img-top" src="ex01.jpeg" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Jonh Ferdinand</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-        card1
+      <div class="card text-white bg-dark">
+        <img class="card-img-top" src="ex01.jpeg" alt="Card image cap">
+        <div class="card-body text-center">
+          <h5 class="card-title">Jonh Ferdinand</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
       </div>
+    </div>
 
-      <div class="col-sm">
-      <div class="card card text-white bg-dark mb-3" style="width: 16rem;">
-  <img class="card-img-top" src="ex02.jpeg" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Ernesto Mateus</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-        card2
+    <div class="col-sm">
+      <div class="card text-white bg-dark">
+        <img class="card-img-top" src="ex02.jpeg" alt="Card image cap">
+        <div class="card-body text-center">
+          <h5 class="card-title">Ernesto Mateus</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
       </div>
+    </div>
 
-      <div class="col-sm">
-      <div class="card card text-white bg-dark mb-3" style="width: 16rem;" >
-  <img class="card-img-top" src="ex03.png.jpeg" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Roberto Carlos</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-        card3
+    <div class="col-sm">
+      <div class="card text-white bg-dark">
+        <img class="card-img-top" src="ex03.png.jpeg" alt="Card image cap">
+        <div class="card-body text-center">
+          <h5 class="card-title">Roberto Carlos</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
       </div>
     </div>
   </div>
-
-
- 
- 
-
- 
-
-
-
-
-
-
-
+</div>
 
 <?php require APPROOT . '/views/includes/footer.php'; ?>
--- app/views/includes/header.php
+++ app/views/includes/header.php
@@ -1,27 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>
-            <?php
-            $title = (VIEW_TITLE !== '') ? VIEW_TITLE . ' | ': '';
-            echo sprintf('%s%s', $title, SITENAME);
-            ?>
-        </title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="<?= URLROOT; ?>css/bootstrap-4.0.0/dist/css/bootstrap.min.css">
-        <link rel="stylesheet" href="<?= URLROOT; ?>css/fontawesome-free-6.4.0-web/css/all.min.css">
-        <link rel="stylesheet" href="<?= URLROOT; ?>/css/style.css">
-        
-    </head>
-
-      
-   
-
-    <body>
-
-    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+<head>
+    <title>
+        <?php
+        $title = (VIEW_TITLE !== '') ? VIEW_TITLE . ' | ' : '';
+        echo sprintf('%s%s', $title, SITENAME);
+        ?>
+    </title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="<?= URLROOT; ?>css/bootstrap-4.0.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= URLROOT; ?>css/fontawesome-free-6.4.0-web/css/all.min.css">
+    <link rel="stylesheet" href="<?= URLROOT; ?>/css/style.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark margin-bottom-padrao">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -35,6 +29,6 @@
                 </li>
             </ul>
         </div>
-    </nav>
-        <?php #require APPROOT . '/views/inc/navbar.php'; ?>
-        <div class="container">
+    </nav>
+    
+    <div class="container">
