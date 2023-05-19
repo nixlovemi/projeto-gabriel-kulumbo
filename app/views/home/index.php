7dab9a2cf82c7916a182b46ba6a50b9ed3dd05ec
Leandro Parra <nixlovemi@gmail.com>
2023-05-18 23:48:27 -0400

    inicio da programacao da home/index

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -12,38 +12,18 @@
 
 <div class="container2 row align-items-center bg-cinza-padrao">
   <div class="row justify-content-between">
-    <div class="col-sm">
-      <div class="card text-white bg-dark">
-        <img class="card-img-top" src="ex01.jpeg" alt="Card image cap">
-        <div class="card-body text-center">
-          <h5 class="card-title">Jonh Ferdinand</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+    <?php foreach($data as $curriculo): ?>
+      <div class="col-sm">
+        <div class="card text-white bg-dark">
+          <img class="card-img-top" src="<?=$curriculo['img']?>" alt="Card image cap">
+          <div class="card-body text-center">
+            <h5 class="card-title"><?php echo $curriculo['nome']; ?></h5>
+            <p class="card-text"><?=$curriculo['funcao']?></p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
         </div>
       </div>
-    </div>
-
-    <div class="col-sm">
-      <div class="card text-white bg-dark">
-        <img class="card-img-top" src="ex02.jpeg" alt="Card image cap">
-        <div class="card-body text-center">
-          <h5 class="card-title">Ernesto Mateus</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-sm">
-      <div class="card text-white bg-dark">
-        <img class="card-img-top" src="ex03.png.jpeg" alt="Card image cap">
-        <div class="card-body text-center">
-          <h5 class="card-title">Roberto Carlos</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    </div>
+    <?php endforeach; ?>
   </div>
 </div>
 
